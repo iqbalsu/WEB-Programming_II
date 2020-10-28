ea79fa9286f14291028731251aa93a8e603319c5
subekhan <iqbalsubekhan98@gmail.com>
2020-10-28 16:43:54 +0700

    first commit

---
+++ Folder 2/praktikum 1/perkenalan.php
@@ -0,0 +1,10 @@
+<?php
+class perkenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Nama saya Iqbal Subekhan<br>
+                saya tinggal di Pulosari<br>
+                Hobi saya makan";
+    }
+
+}
